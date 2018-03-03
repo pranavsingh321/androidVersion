09d97435aca25a497511e461122f10ab7ebc420e
pranavsingh321 <pranavsingh321@gmail.com>
2018-03-03 23:50:28 +0100

    debug changes

--- apkInfo/apkInfo/settings.py
+++ apkInfo/apkInfo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!_o4*39rsvh#f310en)*&bp*09a3o-!&*hct6^+^%j(xvzle-@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
