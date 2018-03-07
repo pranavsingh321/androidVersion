42aa74d34cb2033d5718310419853509322499f7
pranavsingh321 <pranavsingh321@gmail.com>
2018-03-07 07:47:33 +0100

    test case

--- apkInfo/uploadapi/tests.py
+++ apkInfo/uploadapi/tests.py
@@ -2,5 +2,29 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from rest_framework.test import APIClient
+
+from .models import Application
 
 # Create your tests here.
+class ApplicationTestcase(TestCase):
+    """setup for ApplicationTestcase"""
+    def setUp(self):
+        self.application = '/media/test.apk'
+        self.package_name = 'android'
+        self.package_version_code = '4201'
+        self.application_object = Application(application=self.application,
+        	package_name=self.package_name, package_version_code=self.package_version_code)  
+    
+    def test_model_create_application(self):
+        prev_count = Application.objects.count()
+        self.application_object.save()
+        new_count = Application.objects.count()
+        self.assertEqual(prev_count+1, new_count)
+
+class ViewAllApplications(TestCase):
+	"""view  for all ViewAllApplications"""
+    def setUp():
+    	self.client = APIClient()
+    	self.
+		
